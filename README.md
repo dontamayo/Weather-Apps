ab9a6cacd84c6e01cbf78427ff7e8321c197c937
Don aldrich Tamayo <amazingmardon@gmail.com>
2018-01-01 18:17:29 -0500

    what makes up a HTTP request

--- README.md
+++ README.md
@@ -9,3 +9,8 @@
 *  using googleapis
  *  NPM request
 4. Pretty Printing Objects
+5. Whats make up a HTTP Request
+* defined error, responset, body
+* defined header, request
+* statusCode: 200 = means it load successfully
+* error = will be available in statuscode when theres an error with the code etc
--- app.js
+++ app.js
@@ -4,6 +4,12 @@ request({
   url: 'https://maps.googleapis.com/maps/api/geocode/json?address=%20315%20hudson%20st%20Newyork',
   json: true
 }, (error, response, body) => {
-  console.log(JSON.stringify(body, undefined, 2)); //it we will get the entire object list from the terminal
-
+//  console.log(JSON.stringify(response, undefined, 2)); //it we will get the entire object list from the terminal
+console.log(`Address: ${body.results[0].formatted_address}`); //getting the exact adress location from JSON view
+console.log(`Latitude: ${body.results[0].geometry.location.lat}`); // accessing latitute
+console.log(`Longitude: ${body.results[0].geometry.location.lng}`); // acessing longitude
+console.log(`Place id: ${body.results[0].place_id}`);
 });
+// body is the core date that comesback from the server, its the body!
+// response
+//error = means theres something wrong with the code, http address etc
